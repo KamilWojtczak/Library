b9a4fd1d2b4ed2e8288d885996915d64b8034794
Kamil Wojtczak <65222336+KamilWojtczak@users.noreply.github.com>
2020-11-11 12:03:40 +0100

    Update Book.java
    v0.5
    Dodano pakiety, dostosowano kod do wymagań JavaBeans

--- Book.java
+++ Book.java
@@ -1,12 +1,62 @@
-public class Book {
-    String title;
-    String author;
-    int releaseDate;
-    int pages;
-    String publisher;
-    String isbn;
+package pl.javastart.library.model;
 
-    Book(String title, String author, int releaseDate, int pages, String publisher, String isbn) {
+public class Book {
+    private String title;
+    private String author;
+    private int releaseDate;
+    private int pages;
+    private String publisher;
+    private String isbn;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(int releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public Book(String title, String author, int releaseDate, int pages, String publisher, String isbn) {
         this (title, author, releaseDate, pages, publisher);
         this.isbn = isbn;
     }
@@ -19,7 +69,7 @@ public class Book {
         this.publisher = publisher;
     }
 
-    void printInfo(){
+    public void printInfo(){
         String info = title + "; " + author + "; " + releaseDate + "; "
                 + pages + "; " + publisher + "; " + isbn;
         System.out.println(info);
